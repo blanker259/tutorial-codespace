47a104e239495e6c279b24b351beddda09297c5f
blanker259 <blanker259@gmail.com>
2026-09-05 19:02:29 +0000

    Completed the "Numbers in C#" lesson

--- tutorials/numbers.cs
+++ tutorials/numbers.cs
@@ -1,4 +1,107 @@
-int a = 18;
-int b = 6;
-int c = a + b;
-Console.WriteLine(c);
+// WorkWithIntegers();
+//OrderPrecedence();
+//WorkWithDoubles();
+//WorkWithDecimals();
+
+/*void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}
+
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");*/
+
+//Note: you can only use one of these code blocks at a time, otherwise it breaks, i dont know why
+/*void WorkWithDoubles()
+{
+    double a = 5;
+    double b = 4;
+    double c = 2;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
+
+    double a = 19;
+    double b = 23;
+    double c = 8;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
+
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range of double is {min} to {max}");
+
+    double third = 1.0 / 3.0;
+    Console.WriteLine(third);
+}
+*/
+
+/*void WorkWithDecimals()
+{
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"The range of the decimal type is {min} to {max}.");
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
+}
+*/
+
+//program to calculate the area of a circle with a radius of 2.5 centimeters.
+//area = pi * r^2
+double radius = 2.5;
+double area = radius * radius * Math.PI;
+Console.WriteLine($"The area of a circle with a radius of {radius} is: {area}");
