a29146daa05d2725ba900ae12adfd5fb29ad6151
blanker259 <blanker259@gmail.com>
2026-09-10 02:40:26 +0000

    Completed the "Branches and loops" lesson

---
+++ tutorials/branches-loops.cs
@@ -0,0 +1,72 @@
+// ExploreIf();
+
+// static void ExploreIf()
+// {
+//     int a = 5;
+//     int b = 3;
+//     if (a + b > 10)
+//     {
+//         Console.WriteLine("The answer is greater than 10");
+//     }
+//     else
+//     {
+//         Console.WriteLine("The answer is not greater than 10");
+//     }
+
+//     int c = 4;
+//     if ((a + b + c > 10) && (a > b))
+//     {
+//         Console.WriteLine("The answer is greater than 10");
+//         Console.WriteLine("And the first number is greater than the second");
+//     }
+//     else
+//     {
+//         Console.WriteLine("The answer is not greater than 10");
+//         Console.WriteLine("Or the first number is not greater than the second");
+//     }
+
+//     if ((a + b + c > 10) || (a > b))
+//     {
+//         Console.WriteLine("The answer is greater than 10");
+//         Console.WriteLine("Or the first number is greater than the second");
+//     }
+//     else
+//     {
+//         Console.WriteLine("The answer is not greater than 10");
+//         Console.WriteLine("And the first number is not greater than the second");
+//     }
+// }
+
+// int counter = 0;
+// while (counter < 10)
+// {
+//     Console.WriteLine($"Hello World! The counter is {counter}");
+//     counter++;
+// }
+
+// for (int counter = 5; counter < 15; counter++)
+// {
+//     Console.WriteLine($"Hello World! The counter is {counter}");
+// }
+
+// for (int row = 1; row < 11; row ++)
+// {
+//     for (char column = 'a'; column < 'k'; column++)
+//     {
+//         Console.WriteLine($"The cell is ({row}, {column})");
+//     }
+// }
+
+//CHALLENGE
+
+int sum = 0;
+
+for (int num = 1; num < 21; num ++)
+{
+    if (num % 3 == 0)
+    {
+        sum += num;
+    }
+}
+
+Console.WriteLine(sum);
