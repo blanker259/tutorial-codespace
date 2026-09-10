9055087f7807ca5cb7adceab636c4c0733e66cd4
blanker259 <blanker259@gmail.com>
2026-09-10 03:09:48 +0000

    Completed the "List collections" lesson

---
+++ tutorials/lists.cs
@@ -0,0 +1,63 @@
+// //boom boom lets get to work unc
+
+// List<string> names = ["<name>", "Ana", "Felipe"];
+// foreach (var name in names)
+// {
+//     Console.WriteLine($"Hello {name.ToUpper()}!");
+// }
+
+// Console.WriteLine();
+// names.Add("Maria");
+// names.Add("Bill");
+// names.Remove("Ana");
+// foreach (var name in names)
+// {
+//     Console.WriteLine($"Hello {name.ToUpper()}");
+// }
+
+// Console.WriteLine($"My name is {names[0]}.");
+// Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+// Console.WriteLine($"The list has {names.Count} people in it");
+
+// var index = names.IndexOf("Felipe");
+// if (index == -1)
+// {
+//     Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+// }
+// else
+// {
+//     Console.WriteLine($"The name {names[index]} is at index {index}");
+// }
+
+// index = names.IndexOf("Not Found");
+// if (index == -1)
+// {
+//     Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+// }
+// else
+// {
+//     Console.WriteLine($"The name {names[index]} is at index {index}");
+// }
+
+// names.Sort();
+// foreach (var name in names)
+// {
+//     Console.WriteLine($"Hello {name.ToUpper()}!");
+// }
+
+//CHALLENGE
+
+List<int> fibonacciNumbers = [1, 1];
+
+while (fibonacciNumbers.Count < 20)
+{
+    var previous = fibonacciNumbers[^1];
+    var previous2 = fibonacciNumbers[^2];
+
+    fibonacciNumbers.Add(previous + previous2);
+}
+foreach (var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
